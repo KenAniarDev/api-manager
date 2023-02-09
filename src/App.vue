c4d328d02a8705dcb19b1666b4403252547d3326
Nestor Kenneth Aniar <ken.aniar.dev@gmail.com>
2023-02-09 22:43:08 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+// Using ES modules (default)
+import PocketBase from "pocketbase";
+const pb = new PocketBase("http://127.0.0.1:8090/");
+
+// you can also fetch all records at once via getFullList
+const resultList = await pb.collection('endpoint').getList(1, 50, {
+  expand: 'api',
+});
+
+console.log(resultList)
+</script>
+
+<template>
+  <RouterView />
+</template>
